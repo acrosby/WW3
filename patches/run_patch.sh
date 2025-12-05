18f5d2742a368ff85df0eb35167f346c9f8ed453
Alexander Crosby <crosbyar@gmail.com>
2025-12-05 16:58:15 -0500

    clean up run_patch.sh

--- patches/run_patch.sh
+++ patches/run_patch.sh
@@ -5,15 +5,9 @@ shopt -s globstar
 # Run from inside repo/patch directory
 echo The following patches are available:
 echo
-# pushd ../
-# find ./ -type f -name '*.patch' -print0 | sort -z | xargs --null -i echo {}
 find ./ -type f -name '*.patch' -print0 | sort -z | xargs --null -i -n1 bash -c 'echo $1 "<---" $1' sh {} | sed -r 's/\.([a-z|0-9]{7}\.patch)//' | sed -r 's/\.\///g'
 
 echo
 echo
 
 find ./ -type f -name '*.patch' -print0 | sort -z | xargs --null -i -n1 bash -c 'echo ${1} -i ${1}' sh {} | sed -r 's/\.([a-z|0-9]{7}\.patch)//' | sed -r 's/\.\///g' | sed 's/^/..\//' | awk '{print $1" "$2" "$3}' | xargs -t -I{} -n3 patch --verbose -lbu
-
-# patch -lbu
-
-# patch -lbu
